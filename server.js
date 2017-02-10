660e6a7dcc71175299b98fd989154b41f711e864
Ronny Ancorini <Ronnygx@gmail.com>
2017-02-10 21:43:33 +0000

    Small example with mongodb in docker compose

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const mongodb = require('mongodb');
 
-app.get('/', function (req, res) {
+
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/users', (req, res) => {
+	mongodb.connect("mongodb://db:27017", (err, db) => {
+		console.log(err);
+		db.collection("users").find((err, cursor) => {
+			cursor.toArray((err, users) => res.send(users));
+		});
+	});
+})
+
+app.post('/users', (req, res) => {
+	mongodb.connect("mongodb://db:27017", (err, db) => {
+		console.log(err);
+		db.collection("users").insert({usuario: "pepe"}, (err, result) => {
+			if (!err) {
+				res.send("created!");
+			}
+		})
+	});
+})
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
