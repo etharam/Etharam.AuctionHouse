8f2dad11d8b474e60bf5af63b80cc756cb61ee55
Ronny Ancorini <Ronnygx@gmail.com>
2017-04-15 01:40:46 +0100

    green: should notify subscribers when a certain message is published

--- test/eventBus-should.spec.js
+++ test/eventBus-should.spec.js
@@ -3,14 +3,14 @@ require('chai').should();
 const amqp = require('amqplib/callback_api');
 
 function eventBus() {
-    const subscribers = [];
+    const subscribers = {};
     return {
         subscribe({message, subscriber} = {}) {
-            subscribers.push(subscriber);
+            subscribers[message] = [subscriber];
         },
         publish(event) {
             setTimeout(() => {
-                subscribers.forEach(subscriber => subscriber(event.data));
+                subscribers[event.message].forEach(subscriber => subscriber(event.data));
             });
             
         }
@@ -18,18 +18,25 @@ function eventBus() {
 }
 
 describe('Event Bus', () => {
-
-    it('should notify subscribers when a message is emited', (done) => {
+    it('should notify subscribers when a certain message is published', (done) => {
         const bus = eventBus();
+        const MESSAGE = 'A BUS CONCRETE MESSAGE';
         const expectedText = "text message";
+        let nonCalledSubscriberStatus = false;
         const subscriber = (data) => {
             data.text.should.equal(expectedText);
+            nonCalledSubscriberStatus.should.be.false;
             done();
         };
+        const aShouldNonCalledSubscriber = () => {
+            nonCalledSubscriberStatus = true;
+        }
+        bus.subscribe({message: 'Another Message', subscriber: aShouldNonCalledSubscriber});
 
-        bus.subscribe({message: 'Any message', subscriber});
+        bus.subscribe({message: MESSAGE, subscriber});
 
         bus.publish({
+            message: MESSAGE,
             data: { 
                 text: expectedText
             }
