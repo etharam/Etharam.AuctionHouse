55b952de9c323c6f4701e86c2a1c608f949c5b75
Ronny Ancorini <Ronnygx@gmail.com>
2017-08-15 04:04:11 +0200

    does not allow the creation when selling price is less than 1

--- specs/aggregates/auction_spec.py
+++ specs/aggregates/auction_spec.py
@@ -1,4 +1,4 @@
-from expects import expect, equal, have_len
+from expects import expect, equal, have_len, raise_error
 
 from specs.helpers.mamba_keywords import description, it
 
@@ -29,13 +29,23 @@ class Auction:
 
     @classmethod
     def create(cls, auction_id, auctioneer, item, period, selling_price):
+        cls.verify_invariants(selling_price=selling_price)
         auction = Auction(auction_id, auctioneer, item, period, selling_price)
         auction.events.append(auction._create_auction_event())
         return auction
 
+    @classmethod
+    def verify_invariants(cls, selling_price):
+        if selling_price < 1:
+            raise AuctionException()
+
+
+class AuctionException(Exception):
+    pass
+
 
 with description('Auction'):
-    with it('should produce an event of auction created'):
+    with it('produces an event of auction created'):
         auction = Auction.create(auction_id='an_auction_id', auctioneer='an_auctioneer_id', item='an_item_id', period='anything', selling_price=600)
 
         expect(auction.events).to(have_len(1))
@@ -47,3 +57,8 @@ with description('Auction'):
             'period': 'anything',
             'selling_price': 600
         }))
+
+    with it('does not allow the creation when selling price is less than 1'):
+        auction = lambda: Auction.create(auction_id='an_auction_id', auctioneer='an_auctioneer_id', item='an_item_id', period='anything', selling_price=0)
+
+        expect(auction).to(raise_error(AuctionException))
