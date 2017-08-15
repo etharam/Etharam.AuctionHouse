84783b5fdadbe4ffd6422e812f493ad1fc6f71ad
Ronny Ancorini <Ronnygx@gmail.com>
2017-08-15 04:57:20 +0200

    does not allow the creation when expiration date is before today

--- specs/aggregates/auction_spec.py
+++ specs/aggregates/auction_spec.py
@@ -1,14 +1,14 @@
 from expects import expect, equal, have_len, raise_error
 
 from specs.helpers.mamba_keywords import description, context, it
-
+from datetime import date, timedelta
 
 class Auction:
-    def __init__(self, auction_id, auctioneer, item, period, selling_price):
+    def __init__(self, auction_id, auctioneer, item, expiration_date, selling_price):
         self._auction_id = auction_id
         self._auctioneer = auctioneer
         self._item = item
-        self._period = period
+        self._expiration_date = expiration_date
         self._selling_price = selling_price
         self._events = []
         self._create_auction_event()
@@ -23,37 +23,39 @@ class Auction:
             'auction_id': self._auction_id,
             'auctioneer': self._auctioneer,
             'item': self._item,
-            'period': self._period,
+            'expiration_date': self._expiration_date.isoformat(),
             'selling_price': self._selling_price
         }
 
     @classmethod
-    def create(cls, auction_id, auctioneer, item, period, selling_price):
-        cls.verify_invariants(selling_price=selling_price)
-        auction = Auction(auction_id, auctioneer, item, period, selling_price)
+    def create(cls, auction_id, auctioneer, item, expiration_date, selling_price):
+        cls.verify_invariants(selling_price=selling_price, expiration_date=expiration_date)
+        auction = Auction(auction_id, auctioneer, item, expiration_date, selling_price)
         auction.events.append(auction._create_auction_event())
         return auction
 
     @classmethod
-    def verify_invariants(cls, selling_price):
+    def verify_invariants(cls, selling_price, expiration_date):
         if selling_price < 1:
-            raise AuctionException()
+            raise AuctionError()
+        if expiration_date < date.today():
+            raise AuctionError()
 
 
-class AuctionException(Exception):
+class AuctionError(Exception):
     pass
 
 
-def auction_with(price):
+def auction_with(price, expiration_date=date.max):
     return Auction.create(auction_id='an_auction_id', auctioneer='an_auctioneer_id', item='an_item_id',
-                          period='anything', selling_price=price)
+                          expiration_date=expiration_date, selling_price=price)
 
 
 with description('Auction'):
     with context('on creation success'):
         with it('produces an event of auction created'):
             price = 600
-            auction = auction_with(price=price)
+            auction = auction_with(price=price, expiration_date=date.today())
 
             expect(auction.events).to(have_len(1))
             expect(auction.events[0]).to(equal({
@@ -61,7 +63,7 @@ with description('Auction'):
                 'auction_id': 'an_auction_id',
                 'auctioneer': 'an_auctioneer_id',
                 'item': 'an_item_id',
-                'period': 'anything',
+                'expiration_date': date.today().isoformat(),
                 'selling_price': price
             }))
 
@@ -69,4 +71,10 @@ with description('Auction'):
         with it('does not allow the creation when selling price is less than 1'):
             auction = lambda: auction_with(price=0)
 
-            expect(auction).to(raise_error(AuctionException))
+            expect(auction).to(raise_error(AuctionError))
+
+        with it('does not allow the creation when expiration date is before today'):
+            yesterday = date.today() - timedelta(days=1)
+            auction = lambda: auction_with(price=600, expiration_date=yesterday)
+
+            expect(auction).to(raise_error(AuctionError))
