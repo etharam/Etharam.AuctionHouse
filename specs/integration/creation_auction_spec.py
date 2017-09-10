20cf709f77d1867ca4ff1e33aa63c4975f6b2c0a
Miguel Ángel Viera González <mangelviera93@gmail.com>
2017-09-10 03:02:23 +0100

    Green: creates an auction publishing an event in the event bus integration test with Google PubSub

---
+++ specs/integration/creation_auction_spec.py
@@ -0,0 +1,68 @@
+import uuid
+from time import sleep
+
+from arrow import utcnow
+from doublex import Spy, assert_that, called, never
+
+from specs.helpers.mamba_keywords import *
+from src.actions.create_auction import CreateAuction
+
+
+class PubSubMessageBus:
+
+    def __init__(self):
+        from google.cloud import pubsub_v1
+        self.publisher = pubsub_v1.PublisherClient()
+        self.subscriber = pubsub_v1.SubscriberClient()
+
+    def publish(self, event):
+
+        topic_path = self.publisher.topic_path('etharam-179219', 'auction-house')
+        import json
+        self.publisher.publish(topic_path, data=str.encode(json.dumps(event)))
+
+    def subscribe(self, name, callback):
+        subscription = self.subscriber.subscription_path('etharam-179219', 'consumer')
+
+        self.subscriber.subscribe(subscription, callback=callback)
+
+
+class UuidGenerator:
+    def new_id(self):
+        return str(uuid.uuid4())
+
+
+class EventConsumer:
+    def __init__(self, message_bus, event_store):
+        self.message_bus = message_bus
+        self.event_store = event_store
+
+    def listen(self):
+        self.message_bus.subscribe('parrot', callback=self.store)
+
+    def store(self, event):
+        print('parrot')
+        self.event_store.persist(event)
+
+
+class EventStore:
+    def persist(self, event):
+        pass
+
+
+with description('Create auction'):
+    with it('creates an auction publishing an event in the event bus'):
+        fake_event_store = Spy(EventStore)
+        pub_sub_message_bus = PubSubMessageBus()
+        event = {
+            'auctioner': str(uuid.uuid4()),
+            'item': str(uuid.uuid4()),
+            'period': utcnow().floor('day').isoformat(),
+            'selling_price': 600
+        }
+        CreateAuction(pub_sub_message_bus, UuidGenerator()).execute(event)
+
+        EventConsumer(pub_sub_message_bus, fake_event_store).listen()
+
+        sleep(3)
+        assert_that(fake_event_store.persist,  never(called()))
